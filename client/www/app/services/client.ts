75c04b245565c755e661efcbef301e5152caac5f
sdcoffey <sdcoffey@umich.edu>
2017-04-06 18:41:31 -0500

    remove angular

--- client/www/app/services/client.ts
+++
@@ -1,7 +0,0 @@
-import {Observable} from "rxjs/Observable";
-import {NodeInfo} from "../models/nodeinfo";
-
-export interface OlympusClient {
-  listNodes(id: string): Observable<NodeInfo[]>;
-  deleteNode(id: string): Observable<boolean>;
-}
